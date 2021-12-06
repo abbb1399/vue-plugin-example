c79f7535573371f8c71497dd51f5b7ab890408d3
paul <abbb123@naver.com>
2021-12-06 15:12:13 +0900

    플러그인 샘플

---
+++ src/plugins/plugin.js
@@ -0,0 +1,29 @@
+export default{
+  install (Vue, options){
+    
+    Vue.mixin({
+      created(){
+        const { greeting } = options
+        console.log(greeting)
+      }
+    }),
+
+    Vue.prototype.$bold = text =>{
+      return `<b>${text}</b>`
+    }
+  
+    Vue.filter('localeDateString', value =>{
+      if (!(value instanceof Date)) {
+        return value;
+      }
+      return value.toLocaleDateString();
+    })
+
+    Vue.directive('highlight',{
+      inserted(el){
+        el.style.color = 'red'
+      }
+    })
+  
+  }
+}
